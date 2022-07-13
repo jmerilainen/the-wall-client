79528155844f22ac14156abbfa89361331041059
Joonas Meriläinen <merilainenjoonas@gmail.com>
2022-07-13 18:12:34 +0300

    Edit e2e tests for fresh setups

--- cypress/e2e/write.cy.js
+++ cypress/e2e/write.cy.js
@@ -1,52 +1,46 @@
 /// <reference types="cypress" />
 
-describe('The WALL', () => {
+describe("The WALL", () => {
   beforeEach(() => {
-    cy.intercept('GET', '/posts').as('refresh');
-    cy.visit('/')
-    cy.wait('@refresh');
-  })
+    cy.intercept("GET", "/posts").as("refresh");
+    cy.visit("/");
+    cy.wait("@refresh");
+  });
 
-  it('can load the app', () => {
-    cy.get('h1').contains(`WALL`);
-  })
+  it("can load the app", () => {
+    cy.get("h1").contains(`WALL`);
+  });
 
-  it('has an api url', () => {
-    cy.get('[data-test=api_url]').should('not.be.empty');
-  })
+  it("has an api url", () => {
+    cy.get("[data-test=api_url]").should("not.be.empty");
+  });
 
-  it('can add a new entry', () => {
-    cy.intercept('POST', '/posts').as('post');
-    cy.intercept('GET', '/posts').as('refresh');
+  it("can add a new entry", () => {
+    cy.intercept("POST", "/posts").as("post");
+    cy.intercept("GET", "/posts").as("refresh");
 
-    const newItem = 'Cypress was here!'
+    const newItem = "Cypress was here!";
 
-    cy.get('[data-test=new-entry]').type(`${newItem}{enter}`)
+    cy.get("[data-test=new-entry]").type(`First test{enter}`);
+    cy.get("[data-test=new-entry]").type(`${newItem}{enter}`);
 
-    cy.wait('@post');
-    cy.wait('@refresh');
+    cy.wait("@post");
+    cy.wait("@refresh");
 
-    cy.get('[data-test=entry]')
-      .last()
-      .should('contain.text', newItem)
-  })
+    cy.get("[data-test=entry]").last().should("contain.text", newItem);
+  });
 
-  it('can add delete an entry', () => {
-    cy.intercept('DELETE', '/posts/*').as('delete');
-    cy.intercept('GET', '/posts').as('refresh');
+  it("can add delete an entry", () => {
+    cy.intercept("DELETE", "/posts/*").as("delete");
+    cy.intercept("GET", "/posts").as("refresh");
 
-    const newItem = 'Cypress was here!'
+    const newItem = "Cypress was here!";
 
-    cy.get('[data-test=entry]')
-      .last()
-      .find('[data-test=remove]')
-      .click();
+    cy.get("[data-test=entry]").last().find("[data-test=remove]").click();
 
-    cy.wait('@delete');
-    cy.wait('@refresh');
+    cy.wait("@delete");
+    cy.wait("@refresh");
 
-    cy.get('[data-test=entry]')
-      .last()
-      .should('not.contain.text', newItem)
-  })
-})
+    cy.get("[data-test=entry]").last().should("not.contain.text", newItem);
+  });
+});
