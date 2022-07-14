497c075c2f75401983bc2caa72f8e237934dd847
Joonas Meriläinen <merilainenjoonas@gmail.com>
2022-07-14 13:05:57 +0300

    Fix api url

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://the-wall-api.fly.dev';
+const API_URL = import.meta.env.VITE_API_URL;
 
 const api = async (path, args = {}) => {
   const res = await fetch(`${API_URL}${path}`, args);
